997f8e80cbe111631e2ade60c6b718f574e03b62
airmail28 <42902253+airmail28@users.noreply.github.com>
2018-11-27 10:29:40 -0500

    Practice Method 2
    This is the second method, which neatly prints out the terms in the ArrayList.

--- ArrayListPractice.java
+++ ArrayListPractice.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class ArrayListPractice{
 
+	//method 1 to combine lists
 	public static ArrayList<Integer> listCombine(ArrayList<Integer> list1, ArrayList<Integer> list2){
-
 		ArrayList<Integer> combolist = new ArrayList<>();
 		for(int i = 0; i < list1.size(); i++){
 			combolist.add(list1.get(i));
@@ -12,6 +12,13 @@ public class ArrayListPractice{
 			combolist.add(list1.get(i));
 		}
 		return combolist;
+	}
+
+	//method 2 to print out all values of list
+	public static void listPrinter(ArrayList<Integer> list1){
+		for(int i = 0; i < list1.size(); i++){
+			System.out.print(list1.get(i) + ", ");
+		}
 	}
 
 
